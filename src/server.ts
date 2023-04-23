981e92ab7631926cda964262ded1672aa199ae0b
Jamesmoreno21 <jaime.moreno@uc.cl>
2023-04-23 11:16:59 -0400

    feat: add parsers to middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,22 @@
 import App from "./app";
 import dotenv from "dotenv";
 import HomeController from "./controllers/home.controller";
-
+import bodyParser from "body-parser";
 
 // Configure dotenv
 dotenv.config();
 
 const app = new App({
     port: parseInt(process.env.PORT || "3000"),
-    middleWares: [],
-    controllers: [new HomeController()],
+    middleWares: [
+        // Parse JSON request bodies
+        bodyParser.json(), 
+        // Parse URL-encoded request bodies
+        bodyParser.urlencoded({ extended: true }), 
+    ],
+    controllers: [
+        new HomeController()
+    ],
 });
 
 
